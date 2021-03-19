d561bf6e87c5cd75bc4c90f9a8a2c7d12076a564
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-03-19 16:22:46 -0300

    Adding requirements

--- reminder.py
+++ reminder.py
@@ -1,8 +1,10 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime
 
 def reminder():
 	# what do you want to happen every time the job triggers
-
+	# print(now.strftime("%d/%m/%Y %H:%M:%S"))
+	
 scheduler = BlockingScheduler()
 scheduler.add_job(reminder, 'cron', hour='0-23') # available parameters at: https://apscheduler.readthedocs.io/en/stable/modules/triggers/cron.html
 scheduler.start()
