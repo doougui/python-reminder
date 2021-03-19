6537e88b39287ebb19bc27b2a9ed4c73aed49a1c
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-03-19 16:48:22 -0300

    Adding sound on notification

--- reminder.py
+++ reminder.py
@@ -1,6 +1,7 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
 from plyer import notification
+from playsound import playsound
 
 def reminder():
 	now = datetime.now()
@@ -9,7 +10,8 @@ def reminder():
 	    message = "Are you dreaming? Is this real? Pull your finger to find out.",
 	    timeout  = 30
 	)
+	playsound('assets/sound/time-is-now-585.mp3')
 
 scheduler = BlockingScheduler()
-scheduler.add_job(reminder, 'cron', second='0-59') # available parameters at: https://apscheduler.readthedocs.io/en/stable/modules/triggers/cron.html
+scheduler.add_job(reminder, 'cron', hour='0-23') # available parameters at: https://apscheduler.readthedocs.io/en/stable/modules/triggers/cron.html
 scheduler.start()
