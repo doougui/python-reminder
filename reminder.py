f288d1af462db3ec35fa3b77961b4015ad8f7f6c
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-03-19 16:40:14 -0300

    Notifying the user when it's time to do a reality check

--- reminder.py
+++ reminder.py
@@ -1,10 +1,15 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
+from plyer import notification
 
 def reminder():
-	# what do you want to happen every time the job triggers
-	# print(now.strftime("%d/%m/%Y %H:%M:%S"))
-	
+	now = datetime.now()
+	notification.notify(
+	    title = "Reality checker at {}".format(now.strftime("%d/%m/%Y %H:%M:%S")),
+	    message = "Are you dreaming? Is this real? Pull your finger to find out.",
+	    timeout  = 30
+	)
+
 scheduler = BlockingScheduler()
-scheduler.add_job(reminder, 'cron', hour='0-23') # available parameters at: https://apscheduler.readthedocs.io/en/stable/modules/triggers/cron.html
+scheduler.add_job(reminder, 'cron', second='0-59') # available parameters at: https://apscheduler.readthedocs.io/en/stable/modules/triggers/cron.html
 scheduler.start()
